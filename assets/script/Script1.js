d6648fdeb50472efd43e8a1e728a30de2ac71018
Rafael <coppini.rafael2@gmail.com>
2023-03-11 22:37:17 -0300

    first version project

---
+++ assets/script/Script1.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHover: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+            },
+            600: {
+                items: 2,
+                nav: false,
+            },
+            1000: {
+                items: 3,
+                nav: false,
+            }
+        }
+    });
+
+});
+
